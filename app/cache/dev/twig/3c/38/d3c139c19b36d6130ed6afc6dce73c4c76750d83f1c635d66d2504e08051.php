c0fc0e5bfb1b3736f1e9e74e0c4a62ad90e513c1
Antonio Pierro <antonio.pierro@gmail.com>
2014-07-23 09:15:45 +0200

    first commit

---
+++ app/cache/dev/twig/3c/38/d3c139c19b36d6130ed6afc6dce73c4c76750d83f1c635d66d2504e08051.php
@@ -0,0 +1,39 @@
+<?php
+
+/* InnovaRECoreBundle:Default:index.html.twig */
+class __TwigTemplate_3c38d3c139c19b36d6130ed6afc6dce73c4c76750d83f1c635d66d2504e08051 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "InnovaRECoreBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/InnovaRE/CoreBundle/InnovaRECoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace InnovaRE\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InnovaRECoreBundle extends Bundle
+{
+}
